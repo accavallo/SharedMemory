e444058f9b300354a1dd11697b9156d0df0aaa9a
Tony <anthony.c.cavallo@gmail.com>
2016-09-19 18:26:12 -0500

    Initial Commit

---
+++ Proj2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Proj2
+//
+//  Created by Tony on 9/19/16.
+//  Copyright © 2016 Anthony Cavallo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
